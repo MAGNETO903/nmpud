1b3a48ba44a9771dfc95ef1d3e7ae8f8f55c67dd
magneto903 <31848594+MAGNETO903@users.noreply.github.com>
2023-03-30 16:16:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -284,7 +284,7 @@ io.on('connection', function(socket) {
 app.use(express.static(__dirname + '/public'));
 //app.use(bodyParser.urlencoded({extended: false}));
 //app.use(bodyParser.json())
-//app.use(cors());
+app.use(cors());
 
 // запуск сервера
 server.listen(port);
